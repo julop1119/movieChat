fce826522a8f5d35f181f33ae18f54409f722b5e
DESKTOP-QQIDMRA\saassa <julop1119@gmail.com>
2024-01-02 21:23:51 +0900

    no message

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const http = require('node:http');
+'use strict';
 
-const hostname = '127.0.0.1';
-const port = 8010;
+const express = require('express');
 
-const server = http.createServer((req,res)=>{
-	res.statusCode=200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('hellop');
+// 상수
+const PORT = 4004;
+const HOST = '0.0.0.0';
+
+// 앱
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello world\n');
+  console.log(`Running`);
 });
 
-server.listen(port, hostname, ()=>{
-	console.log('asdasdadasdad');
-});
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
